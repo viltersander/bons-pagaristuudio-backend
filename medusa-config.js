3d96b9d42b16c2b9ff08d6d7a7b856758fc1c2c4
viltersander <sander.vilter@ametikool.ee>
2023-05-22 12:55:37 +0300

    initial commit

--- medusa-config.js
+++ medusa-config.js
@@ -40,7 +40,7 @@ const plugins = [
     /** @type {import('@medusajs/admin').PluginOptions} */
     options: {
       autoRebuild: false,
-      url: "bons-pagaristuudio-backend-k7q4sp21p-viltersander.vercel.app",
+      url: "bons-pagaristuudio-backend-th5h-9lin180o3-viltersander.vercel.app",
     },
   },
   {
